4526d162092e5f3eaad42170a4bdd768faba333f
liuyin2004 <liuyin2004@gmail.com>
2015-09-07 19:36:40 +0800

    一步一步跟我学DeviceOne开发 - 仿微信应用(二):底部导航栏 http://bbs.deviceone.net/forum.php?mod=viewthread&tid=201&page=1&extra=#pid298

--- source/default/view/index.ui.js
+++ source/default/view/index.ui.js
@@ -22,21 +22,64 @@ viewshower.addViews([ {
 viewshower.showView("chats");
 
 var button1 = ui("do_Button_1");
+var imageview1 = ui("do_ImageView_1");
+var label1 = ui("do_Label_1");
 button1.on("touch", function() {
-	viewshower.showView("chats");
+	showView("chats");
 });
 
 var button2 = ui("do_Button_2");
+var imageview2 = ui("do_ImageView_2");
+var label2 = ui("do_Label_2");
 button2.on("touch", function() {
-	viewshower.showView("contacts");
+	showView("contacts");
 });
 
 var button3 = ui("do_Button_3");
+var imageview3 = ui("do_ImageView_3");
+var label3 = ui("do_Label_3");
 button3.on("touch", function() {
-	viewshower.showView("discover");
+	showView("discover");
 });
 
 var button4 = ui("do_Button_4");
+var imageview4 = ui("do_ImageView_4");
+var label4 = ui("do_Label_4");
 button4.on("touch", function() {
-	viewshower.showView("me");
+	showView("me");
 });
+
+function showView(name) {
+	viewshower.showView(name);
+	if (name == "chats") {
+		imageview1.source = "source://image/tabbar_mainframeHL.png";
+		label1.fontColor = "09BB07FF";
+	} else {
+		imageview1.source = "source://image/tabbar_mainframe.png";
+		label1.fontColor = "9F9F9FFF";
+	}
+
+	if (name == "contacts") {
+		imageview2.source = "source://image/tabbar_contactsHL.png";
+		label2.fontColor = "09BB07FF";
+	} else {
+		imageview2.source = "source://image/tabbar_contacts.png";
+		label2.fontColor = "9F9F9FFF";
+	}
+
+	if (name == "discover") {
+		imageview3.source = "source://image/tabbar_discoverHL.png";
+		label3.fontColor = "09BB07FF";
+	} else {
+		imageview3.source = "source://image/tabbar_discover.png";
+		label3.fontColor = "9F9F9FFF";
+	}
+
+	if (name == "me") {
+		imageview4.source = "source://image/tabbar_meHL.png";
+		label4.fontColor = "09BB07FF";
+	} else {
+		imageview4.source = "source://image/tabbar_me.png";
+		label4.fontColor = "9F9F9FFF";
+	}
+}
