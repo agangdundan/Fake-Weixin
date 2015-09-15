54053efb36bd60a0db61229ced7838446d0316e9
liuyin2004 <liuyin2004@gmail.com>
2015-09-15 16:48:17 +0800

    http://bbs.deviceone.net/forum.php?mod=viewthread&tid=214&page=1&extra=#pid348 第二个帖

--- source/default/app.js
+++ source/default/app.js
@@ -1,10 +1,15 @@
-/**********************************************
- * Author : @Author
- * Timestamp : @Timestamp
- **********************************************/
+/*******************************************************************************
+ * Author :
+ * 
+ * @Author Timestamp :
+ * @Timestamp
+ ******************************************************************************/
 var d1 = require("deviceone");
 var app = d1.sm("do_App");
 
-app.on("loaded", function () {
-    this.openPage("source://view/index.ui");
+app.on("loaded", function() {
+	this.openPage({
+		source : "source://view/index.ui",
+		statusBarState : "transparent"
+	});
 });
--- source/default/view/chats/index.ui.js
+++ source/default/view/chats/index.ui.js
@@ -1,1 +1,23 @@
 //related to index.ui
+var storage = sm("do_Storage");
+var listdata = mm("do_ListData");
+var listview = ui("listview");
+var json_path = "data://chats/chat.json";//本地缓存的数据
+
+if (storage.fileExist(json_path)) {
+	storage.readFile(json_path, function(data, e) {
+		//deviceone.print(JSON.stringify(data));
+		listdata.addData(data);
+		listview.bindItems(listdata);
+		listview.refreshItems();
+	})
+}
+
+var page = sm("do_Page");
+page.on("loaded",function(){
+	//这个页面加载完显示出来后触发这个事件
+	//我们可以在这个事件里去获取最新的网络数据，来更新listview和data/chats/chat.json
+});
+
+
+
